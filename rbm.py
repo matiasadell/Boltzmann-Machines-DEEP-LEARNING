da495ffea2053e0909af71218a2e9641341ac198
Matias Adell <80477474+matiasadell@users.noreply.github.com>
2022-10-08 23:35:22 -0300

    Update rbm.py

--- rbm.py
+++ rbm.py
@@ -1,10 +1,4 @@
 #Máquinas de Boltzmann Restringidas
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 10 09:51:46 2020
-
-@author: juangabriel
-"""
 
 # Importar las librerías
 import numpy as np
@@ -66,13 +60,13 @@ class RBM():
         self.W = torch.randn(nh, nv)
         self.a = torch.randn(1, nh)
         self.b = torch.randn(1, nv)
-    def sample_h(self, x):           #x = mini_batch_size x nv
-        wx = torch.mm(x, self.W.t()) #mini_batch_size x nh
+    def sample_h(self, x):           
+        wx = torch.mm(x, self.W.t()) 
         activation = wx + self.a.expand_as(wx)
         p_h_given_v = torch.sigmoid(activation)
         return p_h_given_v, torch.bernoulli(p_h_given_v)
-    def sample_v(self, y):           #y = mini_batch_size x nh
-        wy = torch.mm(y, self.W) #mini_batch_size x nv
+    def sample_v(self, y):           
+        wy = torch.mm(y, self.W) 
         activation = wy + self.b.expand_as(wy)
         p_v_given_h = torch.sigmoid(activation)
         return p_v_given_h, torch.bernoulli(p_v_given_h)   
